a3f1830d9b89db041a6c109b862f53775164da05
yadavAnkit12 <ay1222926@gmail.com>
2024-03-14 16:32:01 +0530

    adding animation

--- src/components/Contact/Brochure.jsx
+++ src/components/Contact/Brochure.jsx
@@ -3,8 +3,8 @@ import img from '../../assets/Img.jpg'
 import { Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
 import SouthIcon from '@mui/icons-material/South';
-import { motion } from "framer-motion"
 import Reveal from '../../utils/Reveal';
+import {motion} from "framer-motion"
 
 const Brochure = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -26,32 +26,15 @@ const Brochure = () => {
   }
 
   return (
-    <div style={{ padding: '1rem 6rem' }}>
-      <Grid container style={{ margin: '0 auto' }}>
-        {/* Left Side: Image */}
-        <Grid item xs={12} md={6} >
-          <div style={{ position: 'relative', marginBottom: '3rem' }}>
-            <motion.div
-              initial={{
-                opacity: 0,
-                x: -100,
-              }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  duration: 1,
-                },
-              }}
-              viewport={{ once: true }}
-
-            >
-              <img src={img} alt="Your Image" style={{ minHeight: '400px', minWidth: '300px', width: '100%', maxHeight: '100%', objectFit: 'cover' }} />
-              {/* Button */}
+    <>
+        <div style={{ padding: '10%' }}>
+          <Grid container spacing={5}>
+            {/* Left Side: Image */}
+            <Grid item xs={12} md={6} >
               <motion.div
                 initial={{
                   opacity: 0,
-                  x: -20,
+                  x: -100,
                 }}
                 whileInView={{
                   opacity: 1,
@@ -60,61 +43,59 @@ const Brochure = () => {
                     duration: 1,
                   },
                 }}
-                viewport={{ once: true }}
+                viewport={{ once: true }}>
 
-              >
+                <div style={{ position: 'relative' }}>
+                  <img src={img} alt="Your Image" style={{ width: '100%', maxHeight: '100%', objectFit: 'cover' }} />
 
-                <Link
-                  to="https://25king.ca/brochure/"
-                  target='_blank'
-                  style={btn}
-                  onMouseEnter={() => setIsHovered(true)}
-                  onMouseLeave={() => setIsHovered(false)}
-                >
-                  View Brochure <SouthIcon />
-                </Link>
+                  {/* Button */}
+
+                  <Link
+                    to="https://25king.ca/brochure/"
+                    target='_blank'
+                    style={btn}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                  >
+                    View Brochure <SouthIcon />
+                  </Link>
+
+                </div>
               </motion.div>
-            </motion.div>
+            </Grid>
 
+            {/* Right Side: Text */}
+            <Grid item xs={12} md={6} style={{ marginTop: '8%' }}  >
+            <motion.div
+                initial={{
+                  opacity: 0,
+                  x: 100,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                  transition: {
+                    duration: 1,
+                  },
+                }}
+                viewport={{ once: true }}>
 
-          </div>
-        </Grid>
-
-        {/* Right Side: Text */}
-        <Grid item xs={12} md={6} container justifyContent="center" alignItems="center">
-          <motion.div
-            initial={{
-              opacity: 0,
-              x: 100,
-            }}
-            whileInView={{
-              opacity: 1,
-              x: 0,
-              transition: {
-                duration: 1,
-              },
-            }}
-            viewport={{ once: true }}
-            style={{ textAlign: 'start' }} // align the content to the center horizontally
-          >
-            <div> {/* this div should be centered vertically and horizontally */}
-              <p style={{ wordWrap: 'break-word' }}>For further information on this exclusive leasing opportunity<br />please contact:</p>
+              <p>For further information on this exclusive leasing opportunity<br />please contact:</p>
               <div className='flex flex-col my-4'>
                 <strong>KATE LABRASH</strong>
-                <span style={{ color: '#a57c52', textDecoration: 'underline', wordWrap: 'break-word' }}>DIRECTOR, OFFICE LEASING/SALES REPRESENTATIVE</span>
+                <span style={{ color: '#a57c52', textDecoration: 'underline' }}>DIRECTOR, OFFICE LEASING/SALES REPRESENTATIVE</span>
               </div>
 
-              <strong>OFFICE:</strong>
-              <Link to='tel: 416-673-7491' style={{ color: '#a57c52', textDecoration: 'underline' }}>416-673-7491</Link> <br />
+              <strong >OFFICE:</strong>
+              <Link to='tel: 416-673-7491' style={{ color: '#a57c52', textDecoration: 'underline' }}>416-673-7491</Link>  <br />
 
-              <strong>EMAIL:</strong>
-              <Link to='mailto:kate.labrash@quadreal.com' style={{ color: '#a57c52', textDecoration: 'underline', wordWrap: 'break-word' }}>KATE.LABRASH@QUADREAL.COM</Link>
-            </div>
-          </motion.div>
-        </Grid>
-
-      </Grid>
-    </div>
+              <strong >EMAIL:</strong>
+              <Link to='mailto:kate.labrash@quadreal.com' style={{ color: '#a57c52', textDecoration: 'underline' }}>KATE.LABRASH@QUADREAL.COM</Link>
+                </motion.div>
+            </Grid>
+          </Grid>
+        </div>
+    </>
   )
 }
 
